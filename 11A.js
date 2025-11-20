c584f1b019bef8d5b70b831190b18738e1e832ea
ptrabyu123-cmyk <ptrabyu123@gmail.com>
2025-11-20 19:53:00 +0700

    Create 11A.js

---
+++ 11A.js
@@ -0,0 +1,16 @@
+let mahasiswa = new Map();
+ mahasiswa.set("001", "Andi");
+ mahasiswa.set("002", "Budi");
+  mahasiswa.set("003", "vera");
+  mahasiswa.set("004", "putra");
+  mahasiswa.set("002", "Budi");
+  mahasiswa.set("002", "vera");
+  
+  console.log(mahasiswa.get("001")); // Output: Andi            
+  console.log(mahasiswa.get("001")); // Output: Andi
+ 
+  let mahasiswi = ["Andi", "Budi", "Citra", "Dewi"];
+ console.log(mahasiswi[0]); // Output: Andi
+ 
+  let angka = new Set([1, 2, 3, 3, 4, 5, 5]);
+ console.log(angka); // Output: {1, 2, 3, 4, 5}
